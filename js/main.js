32504b6e32f443bffcad01e82b0b399cc20ffec4
shaifullah599 <83369080+shaifullah599@users.noreply.github.com>
2021-11-10 18:10:14 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+
+$('.sec-5').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  arrows:false,
+});
+		
+
+
+$('.sec-6').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  prevArrow: '<i class="fas fa-arrow-circle-left"></i>',
+  nextArrow:'<i class="fas fa-arrow-circle-right"></i>',
+  
+});
+
+$('.sec-7').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  prevArrow: '<i class="fas fa-arrow-circle-left"></i>',
+  nextArrow:'<i class="fas fa-arrow-circle-right"></i>',
+  
+});
+
+
+$('.sec-8').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  prevArrow: '<i class="fas fa-arrow-circle-left"></i>',
+  nextArrow:'<i class="fas fa-arrow-circle-right"></i>',
+  
+});
+
+
+
+
+
+
